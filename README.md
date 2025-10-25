ff5f161d0af3a80adba96fb337c41951ce04d8ae
zemguimaraes <josemaria_vasconcelosguimaraes@hotmail.com>
2025-10-25 16:34:10 +0000

    Initial commit: php project

---
+++ README.md
@@ -0,0 +1,17 @@
+# fijahu-4
+
+A simple Php project.
+
+## Description
+This is an auto-generated project demonstrating basic php functionality.
+
+## Running
+```bash
+# See the main file: index.php
+```
+
+## Language
+- Php
+
+## License
+MIT
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * fijahu-4 - PHP Implementation
+ * A simple Hello World program
+ */
+
+function main() {
+    echo "Hello, World!\n";
+    echo "Welcome to fijahu-4\n";
+    
+    // Simple calculation
+    $numbers = [1, 2, 3, 4, 5];
+    $total = array_sum($numbers);
+    echo "Sum of numbers: $total\n";
+}
+
+main();
+?>
